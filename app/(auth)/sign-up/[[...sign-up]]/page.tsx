af2f16f2b646c0b66d82ec6b4f993cf922f697c4
Kushan Yasiru <52574258+Kushanyasiru97@users.noreply.github.com>
2025-03-26 03:27:30 +0530

    Home Page development started

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const  SignUpPage = () => {
   return (
-    <main className='flex h-screen w-full items-centerjustify-center'>
+    <main className='flex h-screen w-full my-5 items-center justify-center'>
         <SignUp />
     </main>
   )
